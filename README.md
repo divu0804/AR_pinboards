6e7c5dc3dce8e34e692f0a0207227ad1db1d62da
Divya Reddy <divu0804@gmail.com>
2023-09-19 20:29:25 -0400

    all

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# arPinboard
-A web application to implement pinboards using VR/AR
+## AR Pinboards
+We created AR pinboards as our final year project in college, It is a responsive web app using three js that allows users to interact and publish their pinboards using augmented reality.
+
+- Developed using a markerless AR where a link or QR code is used to access a pinboard.
+
+- Paper publication: Click [here](https://www.tojqi.net/index.php/journal/article/view/1172/519) to read.
+<img src="assets/readme_images/Screenshot 2022-06-10 at 2.08.03 PM.png">
+
+<hr>
+
+## Live Project
+Follow the link to check out the demo app [here](https://thebenezer.github.io/pinboARds/)
+
+<hr>
+
+
+## Demo
+
+https://user-images.githubusercontent.com/54198861/176197306-803457c1-a460-41cb-8da8-0fb6940ae155.mp4
+
+
+<hr>
+
+## Tools Used
+<img src="https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white">
+<img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E">
+<img src="https://img.shields.io/badge/threejs-black?style=for-the-badge&logo=three.js&logoColor=white">
+<img src="https://img.shields.io/badge/GoogleCloud-%234285F4.svg?style=for-the-badge&logo=google-cloud&logoColor=white">
+<img src="https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white">
+<hr>
--- js/menu.js
+++ js/menu.js
@@ -3,90 +3,92 @@ const hamburger =document.querySelector('.hamburger');
 const line1 =document.querySelector('.line1');
 const line2 =document.querySelector('.line2');
 const line3 =document.querySelector('.line3');
-const x =document.querySelector('.x');
-const scrollToTop =document.querySelector('.scroll-to-top');
+const navbar =document.querySelector('.navbar');
+const nav =document.querySelector('.nav');
 const navlinks =document.querySelector('.nav-links');
-const login= document.querySelector('.login');
-const nav= document.querySelector('nav');
+// const scrollToTop =document.querySelector('.scroll-to-top');
+// const login= document.querySelector('.login');
 const logo= document.querySelector('.logo img');
 const links = document.querySelectorAll('.nav-links li');
 // const navbuttons = document.querySelectorAll('.nav-buttons');
 
 hamburger.addEventListener('click',()=>{
-  if (navlinks.classList.contains('open')) {
+  if (nav.classList.contains('open')) {
+    navbar.classList.remove('open');
+    nav.classList.remove('open');
     navlinks.classList.remove('open');
-    setTimeout(() => {  navlinks.classList.remove('display'); }, 2000);
+    // setTimeout(() => {  nav.classList.remove('display'); }, 2000);
   } else {
-    navlinks.classList.add('display');
-    setTimeout(() => {  navlinks.classList.add('open'); }, 10);
+    setTimeout(() => {  nav.classList.add('open'); }, 10);
+    navlinks.classList.add('open');
+    navbar.classList.add('open');
     
   }
     line1.classList.toggle('close');
     line2.classList.toggle('close');
     line3.classList.toggle('close');
-    scrollToTop.classList.toggle('close');
-    x.classList.toggle('open');
+    // scrollToTop.classList.toggle('close');
     links.forEach(link =>{
         link.classList.toggle('fade');
     });
-    login.classList.toggle('fade');
+    // login.classList.toggle('fade');
 });
 
 
 //..........SCROLL BUTTONS..........
 
-const more= document.querySelector('.more');
+// const more= document.querySelector('.more');
 
-window.addEventListener("scroll", scrollFunction);
+// window.addEventListener("scroll", scrollFunction);
 
-function scrollFunction() {
+// function scrollFunction() {
  
-    if (window.pageYOffset > 40) { // Make logo small and change nav colour
-        logo.classList.add('small');
-        nav.classList.add('small');
-        more.classList.add('fade');
-      }
-    else { // make logo normal
-      nav.classList.remove('small');
-      logo.classList.remove('small');
-      more.classList.remove('fade');
-    }
-    if (window.pageYOffset > 300) { // Show scrollToTop
-        scrollToTop.classList.add('fade');
-        // scrollToTop.style.display = "block";
-    }
-    else { // Hide scrollToTop
-        scrollToTop.classList.remove('fade');
-        // scrollToTop.style.display = "none";
-    }
-}
+//     if (window.pageYOffset > 40) { // Make logo small and change nav colour
+//         logo.classList.add('small');
+//         nav.classList.add('small');
+//         more.classList.add('fade');
+//       }
+//     else { // make logo normal
+//       nav.classList.remove('small');
+//       logo.classList.remove('small');
+//       more.classList.remove('fade');
+//     }
+//     if (window.pageYOffset > 300) { // Show scrollToTop
+//         scrollToTop.classList.add('fade');
+//         // scrollToTop.style.display = "block";
+//     }
+//     else { // Hide scrollToTop
+//         scrollToTop.classList.remove('fade');
+//         // scrollToTop.style.display = "none";
+//     }
+// }
 
-scrollToTop.addEventListener("click", smoothScrollBackToTop);
+// scrollToTop.addEventListener("click", smoothScrollBackToTop);
 
 
-function smoothScrollBackToTop() {
-  const targetPosition = 0;
-  const startPosition = window.pageYOffset;
-  const distance = targetPosition - startPosition;
-  const duration = 750;
-  let start = null;
+// function smoothScrollBackToTop() {
+//   const targetPosition = 0;
+//   const startPosition = window.pageYOffset;
+//   const distance = targetPosition - startPosition;
+//   const duration = 750;
+//   let start = null;
   
-  window.requestAnimationFrame(step);
+//   window.requestAnimationFrame(step);
 
-  function step(timestamp) {
-    if (!start) start = timestamp;
-    const progress = timestamp - start;
-    window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
-    if (progress < duration) window.requestAnimationFrame(step);
-  }
-}
+//   function step(timestamp) {
+//     if (!start) start = timestamp;
+//     const progress = timestamp - start;
+//     window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+//     if (progress < duration) window.requestAnimationFrame(step);
+//   }
+// }
 
-function easeInOutCubic(t, b, c, d) {
-	t /= d/2;
-	if (t < 1) return c/2*t*t*t + b;
-	t -= 2;
-	return c/2*(t*t*t + 2) + b;
-};
+// function easeInOutCubic(t, b, c, d) {
+// 	t /= d/2;
+// 	if (t < 1) return c/2*t*t*t + b;
+// 	t -= 2;
+// 	return c/2*(t*t*t + 2) + b;
+// };
 
 // window.onscroll = function(ev) {
 //   if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight-20) {
